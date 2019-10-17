457d5940118dc7333490d0927bfa3b0f6c6dc8d8
barunbizit <56625816+barunbizit@users.noreply.github.com>
2019-10-17 13:59:17 +0900

    Add files via upload

---
+++ precache-manifest.558b8760677b037424f3f9d011be6049.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888cf6d643f6c19a1f47",
+    "url": "/css/app.a510f641.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "500a7b546e7db4daffd3f68a098464f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888cf6d643f6c19a1f47",
+    "url": "/js/app.4780ecde.js"
+  },
+  {
+    "revision": "1a71bd5259b43fee7f30",
+    "url": "/js/chunk-vendors.a523d01f.js"
+  },
+  {
+    "revision": "4eac1914dd8800e8d8e4b1202caa43f9",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.46fb8e581b1d508197c205d8111244b5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.558b8760677b037424f3f9d011be6049.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* workbox - sw */
 
@@ -18,38 +18,3 @@ self.addEventListener('message', (event) => {
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
-
-/* firebase - sw */
-importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCE9K0X8StAgnxypSg8QTeVs6Mvh7CWHfo",
-  authDomain: "pwatest-c4ed0.firebaseapp.com",
-  databaseURL: "https://pwatest-c4ed0.firebaseio.com",
-  projectId: "pwatest-c4ed0",
-  storageBucket: "pwatest-c4ed0.appspot.com",
-  messagingSenderId: "218708421207",
-  appId: "1:218708421207:web:bcd7ad0373d91bef0b1112"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const messaging = firebase.messaging();
-
-messaging.onMessage((payload) => {
-  console.log('Message received. ', payload);
-});
-
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  return self.registration.showNotification(notificationTitle,
-    notificationOptions);
-});
