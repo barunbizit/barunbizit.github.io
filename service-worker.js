ab086129716eff3a59dab0bec8eb97820b7278df
barunbizit <56625816+barunbizit@users.noreply.github.com>
2019-10-17 13:52:31 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,11 @@
-importScripts("/precache-manifest.2cf3246bff7cc1b897b852bbd65f0cd7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.46fb8e581b1d508197c205d8111244b5.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+/* workbox - sw */
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e2f7511cac76efe898b50b9a1ce410ba.js"
+  "/precache-manifest.46fb8e581b1d508197c205d8111244b5.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vuepwa"});
@@ -27,11 +16,40 @@ self.addEventListener('message', (event) => {
   }
 });
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
+/* firebase - sw */
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCE9K0X8StAgnxypSg8QTeVs6Mvh7CWHfo",
+  authDomain: "pwatest-c4ed0.firebaseapp.com",
+  databaseURL: "https://pwatest-c4ed0.firebaseio.com",
+  projectId: "pwatest-c4ed0",
+  storageBucket: "pwatest-c4ed0.appspot.com",
+  messagingSenderId: "218708421207",
+  appId: "1:218708421207:web:bcd7ad0373d91bef0b1112"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+});
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.',
+    icon: '/firebase-logo.png'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
