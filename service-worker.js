6ab5373344402f7276c85bf2bf34071a1db0e8c0
barunbizit <56625816+barunbizit@users.noreply.github.com>
2019-10-17 14:12:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,6 @@ importScripts("/precache-manifest.205376fb7c4bf390d4dd2b62883aed69.js", "https:/
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-importScripts(
-  "/precache-manifest.46fb8e581b1d508197c205d8111244b5.js"
-);
-
 workbox.core.setCacheNameDetails({prefix: "vuepwa"});
 
 self.addEventListener('message', (event) => {
