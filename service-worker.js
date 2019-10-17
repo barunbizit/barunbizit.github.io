75aa2712545e5d92fa3ff4e6225c642180048465
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-17 15:44:25 +0900

    ok3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.5aea02fabf221242d2b59df24b8d64d1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.12959876b42a85ca02388c9d96e665f8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* workbox - sw */
 
