19984481b7251b917c027f5a676af98d00f09f28
barunbizit <56625816+barunbizit@users.noreply.github.com>
2019-10-17 09:58:54 +0900

    Add files via upload

---
+++ precache-manifest.8da78ebf9c090c1e1b7fb4d726026692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "05bd2f7f1bc08864ed3c",
+    "url": "/js/chunk-vendors.ac7b1dcd.js"
+  },
+  {
+    "revision": "d307811a6a0159071bfd",
+    "url": "/js/app.783519ac.js"
+  },
+  {
+    "revision": "7013bd209d44f1f9e256b5c28b87fbe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d307811a6a0159071bfd",
+    "url": "/css/app.6af8ca07.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-importScripts("/precache-manifest.4af7e9de267f62fa56ec024cccbe971e.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.8da78ebf9c090c1e1b7fb4d726026692.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 self.addEventListener('push', function (event) {
+  console.log('push !');
   let push_message = event.data.json();
   const options = {
     body : push_message.notification.body,
@@ -16,6 +17,7 @@ self.addEventListener('push', function (event) {
 });
 
 self.addEventListener('notificationclick', function (event) {
+  console.log('notification click !');
   const clicked = event.notification;
   clicked.close();
   
