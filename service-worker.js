ed6bf3f35c1f8ef9fd824282f70e226ea6552e65
barunbizit <56625816+barunbizit@users.noreply.github.com>
2019-10-17 10:13:33 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8da78ebf9c090c1e1b7fb4d726026692.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.219b65d5fbf59cd8e4f9a171159caa6d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
