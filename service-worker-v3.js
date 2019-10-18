07712852b5cd8eae8c9b55bb8d5bb5dda0e4916d
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-18 15:26:25 +0900

    ok

---
+++ service-worker-v3.js
@@ -0,0 +1,14 @@
+importScripts("/precache-manifest.52b3067b320658a27335e00d6ba60405.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.setCacheNameDetails({prefix: "vuepwa3"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
