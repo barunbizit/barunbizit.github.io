ef931cc6e237f847b566d4b1a046d69df3d60b02
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-18 17:08:27 +0900

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-importScripts("/precache-manifest.3e6f8e2c2bae599880d041121a8abb5a.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.624c7db33bd2db1a7312fa8006149584.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-workbox.core.setCacheNameDetails({prefix: "vuepwa"});
+workbox.core.setCacheNameDetails({prefix: "swcache-v1"});
 
 self.addEventListener('install', (event) => {
   console.log('service-worker install . . .');
@@ -16,6 +16,12 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('message', (event) => {
   console.log('service-worker message . . . ', event);
+});
+
+self.addEventListener('push', (event) => {
+  console.log('service-worker push . . . ' + event.data.text());
+
+  event.waitUntil(self.registration.showNotification('PUSH', {body:event.data.text()}));
 });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
