ac264814848f25597f9f5bec942891f8ad772d60
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-18 15:42:25 +0900

    ok

---
+++ precache-manifest.97f3aba902b48ccfe2d1d3ad155836d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "106c6606965fe2aec8f3",
+    "url": "/css/app.e6b2d714.css"
+  },
+  {
+    "revision": "6fca98c8aab8a7cb494cfd70cde84025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "106c6606965fe2aec8f3",
+    "url": "/js/app.94b94956.js"
+  },
+  {
+    "revision": "1ad732e59eceecdc1789",
+    "url": "/js/chunk-vendors.ee3a43c1.js"
+  },
+  {
+    "revision": "0e17f61d5b89dce2b857a02498700546",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,14 @@
-importScripts("/precache-manifest.f8f04c2dae4de9643b522c7ba83bcd50.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.97f3aba902b48ccfe2d1d3ad155836d8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "vuepwa"});
+
+self.addEventListener('install', (event) => {
+  console.log('service-worker install ..');
+
+  self.skipWaiting();
+});
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
