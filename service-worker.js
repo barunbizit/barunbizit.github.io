c9032a7ce41c623d69ab0255b8749a97935c618a
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-18 15:44:50 +0900

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-importScripts("/precache-manifest.97f3aba902b48ccfe2d1d3ad155836d8.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.38c998ba10768ab5cb0612f4467afa99.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "vuepwa"});
 
 self.addEventListener('install', (event) => {
-  console.log('service-worker install ..');
+  console.log('service-worker install ....');
 
   self.skipWaiting();
 });
