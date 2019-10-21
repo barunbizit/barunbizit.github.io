8e871cc2d8c3e81351c197d0f9243b85adb1e716
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-21 15:32:44 +0900

    ok

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/6.3.4/firebase-messaging.js');
 
-var firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyCE9K0X8StAgnxypSg8QTeVs6Mvh7CWHfo",
   authDomain: "pwatest-c4ed0.firebaseapp.com",
   databaseURL: "https://pwatest-c4ed0.firebaseio.com",
@@ -10,8 +10,7 @@ var firebaseConfig = {
   messagingSenderId: "218708421207",
   appId: "1:218708421207:web:bcd7ad0373d91bef0b1112",
   measurementId: "G-93VWW5Y93Q"
-};
-firebase.initializeApp(firebaseConfig);
+});
 
 const messaging = firebase.messaging();
 
