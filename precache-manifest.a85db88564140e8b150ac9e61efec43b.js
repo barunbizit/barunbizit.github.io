a5842161dcce51c81dc4494a8459e6e0485659d4
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-21 14:19:18 +0900

    ok

---
+++ precache-manifest.a85db88564140e8b150ac9e61efec43b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1071668add4f47376a1",
+    "url": "/css/app.e6b2d714.css"
+  },
+  {
+    "revision": "97e1e3d195354f7c0c9bf56a4fc8c189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1071668add4f47376a1",
+    "url": "/js/app.ece6e69a.js"
+  },
+  {
+    "revision": "98316f297b10955c73fb",
+    "url": "/js/chunk-vendors.14dd40fb.js"
+  },
+  {
+    "revision": "0e17f61d5b89dce2b857a02498700546",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.8e4a6900f33fbe5c0aa4a45db240fd11.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.a85db88564140e8b150ac9e61efec43b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "swcache-v1"});
 
