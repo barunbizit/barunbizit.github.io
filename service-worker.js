d5360d77538ea4ada94688a6113016e614bfa388
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-21 17:25:29 +0900

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.38d74f3c6aadff47bb900984d1572767.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.d44b813b394fbf3edc0fac974be547a0.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "swcache-v1"});
 
@@ -14,11 +14,11 @@ self.addEventListener('activate', (event) => {
   self.clients.claim();
 });
 
-self.addEventListener('push', (event) => {
-  console.info('[info] 서비스워커 알림 : ' + event);
+// self.addEventListener('push', (event) => {
+//   console.info('[info] 서비스워커 알림 : ' + event);
 
-  event.waitUntil(self.registration.showNotification('PUSH', {body:event.data.text()}));
-});
+//   event.waitUntil(self.registration.showNotification('PUSH', {body:event.data.text()}));
+// });
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
