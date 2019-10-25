24f8dd56c801dacd14623a748c3acd83656a9241
barunbizit <barunbizit@gmail.com>
2019-10-25 15:27:48 +0900

    ok

---
+++ precache-manifest.025dc7eac21248c6bce2833fc8f6e1fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74388d66451b8009291f",
+    "url": "/css/app.6af8ca07.css"
+  },
+  {
+    "revision": "7451847e939a12946b2a83390fe3249d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74388d66451b8009291f",
+    "url": "/js/app.03a99fe9.js"
+  },
+  {
+    "revision": "5dbceab410aca59e9228",
+    "url": "/js/chunk-vendors.68d32d5b.js"
+  },
+  {
+    "revision": "7d065064b1508ff4047c7d8b4fd98709",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.b3b3e160e0fc2593a54e5ea2089b6293.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.025dc7eac21248c6bce2833fc8f6e1fa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "pwa"});
 
@@ -8,11 +8,10 @@ self.addEventListener('message', (event) => {
   }
 });
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
+workbox.routing.registerRoute(/.*(?:barunbizit)*$/, new workbox.strategies.NetworkFirst({
+  cacheName:'barunbizit'
+}));
+
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
