44d888f4c93f087b7813684cfbaebee939c52894
이진희 <devcodeits@ijinhuiui-MacBookPro.local>
2019-10-25 13:43:06 +0900

    캐싱전략

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,27 @@
 importScripts("/precache-manifest.736ee4277463478fc0f7ae92d97c1ddf.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-workbox.core.setCacheNameDetails({prefix: "pwa"});
+workbox.loadModule("workbox-strategies");
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.skipWaiting();
+workbox.clientsClaim();
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+workbox.routing.registerRoute(routeData => {
+  return routeData.event.request.headers.get("accept").includes("text/html");
+}, workbox.strategies.networkFirst());
+
+// workbox.core.setCacheNameDetails({prefix: "pwa"});
+
+// self.addEventListener('message', (event) => {
+//   if (event.data && event.data.type === 'SKIP_WAITING') {
+//     self.skipWaiting();
+//   }
+// });
+
+// /**
+//  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+//  * requests for URLs in the manifest.
+//  * See https://goo.gl/S9QRab
+//  */
+// self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
