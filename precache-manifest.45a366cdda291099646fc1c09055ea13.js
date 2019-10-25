ea36cabc21cb0d70bc21a96ee18fd9b4cfecd513
barunbizit <barunbizit@gmail.com>
2019-10-25 15:40:43 +0900

    ok

---
+++ precache-manifest.45a366cdda291099646fc1c09055ea13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fb5e964fef391967d8",
+    "url": "/css/app.6af8ca07.css"
+  },
+  {
+    "revision": "86c0578f79f42704e499b109c7e623eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72fb5e964fef391967d8",
+    "url": "/js/app.d5b82d5c.js"
+  },
+  {
+    "revision": "d46c92efe3b13ee5c915",
+    "url": "/js/chunk-vendors.b75d1200.js"
+  },
+  {
+    "revision": "7d065064b1508ff4047c7d8b4fd98709",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c2cdfb4dde10f166289ce4fd03fca6ce.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.45a366cdda291099646fc1c09055ea13.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "pwa"});
 
