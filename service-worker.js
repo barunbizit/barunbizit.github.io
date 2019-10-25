ece96cd1e6e6922892475c103574937643520e35
barunbizit <barunbizit@gmail.com>
2019-10-25 15:29:43 +0900

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.025dc7eac21248c6bce2833fc8f6e1fa.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.d0b2027b34ec40acf25d08dc90a1575c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.core.setCacheNameDetails({prefix: "pwa"});
 
